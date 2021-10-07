dcb85ccb7ff1e45e7c6f85532f24eddef44727b7
Oleh Vysotskyj <ovysockij2@gmail.com>
2021-10-07 11:31:29 +0300

    updated search to work with asc and desc lists

--- src/main/scala/binary/BinarySearchExample.scala
+++ src/main/scala/binary/BinarySearchExample.scala
@@ -9,10 +9,24 @@ class BinarySearchExample {
       val mid = Math.floor((low + high) / 2).toInt
       val guess = list(mid)
       guess.toString.compare(item.toString) match {
-        case 1 =>
+        case x if x > 0 =>
           high = mid - 1
-        case -1 =>
+        case x if x < 0 =>
           low = mid + 1
+        case 0 =>
+          return Some(mid)
+      }
+    }
+    low = 0
+    high = list.size - 1
+    while (low <= high) {
+      val mid = Math.floor((low + high) / 2).toInt
+      val guess = list(mid)
+      guess.toString.compare(item.toString) match {
+        case x if x > 0 =>
+          low = mid + 1
+        case x if x < 0 =>
+          high = mid - 1
         case 0 =>
           return Some(mid)
       }
